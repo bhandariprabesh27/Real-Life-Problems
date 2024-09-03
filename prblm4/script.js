7a851f7a237ae5416c16af66358e9d20662f54bf
Prabesh Bhandari <prabesh.chitwan@gmail.com>
2024-09-03 22:01:52 +0545

    final commit

--- prblm4/script.js
+++ prblm4/script.js
@@ -1,1 +1,7 @@
 //Custom Tooltip
+document.querySelector("#btn").addEventListener("mouseenter", function () {
+  document.querySelector(".tooltip").style.display = "initial";
+});
+document.querySelector("#btn").addEventListener("mouseleave", function () {
+  document.querySelector(".tooltip").style.display = "none";
+});
